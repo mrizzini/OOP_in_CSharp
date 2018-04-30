37c610a37dfb500187d8102346aa2a82cd771f51
Matthew Rizzini <matthewrizzini@gmail.com>
2018-04-30 19:49:45 -0400

    starting Module 1

---
+++ Practice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Intro_to_OOP__EDX_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DrinksMachine myMachine = new DrinksMachine("Kitchen", "Brand", "DM1000");
+            // // myMachine.Location = "Kitchen";
+            // // myMachine.Model = "DM1000";
+
+            // Console.WriteLine(myMachine.Location);
+            // Console.WriteLine(myMachine.Make);
+            // Console.WriteLine(myMachine.Model);
+
+            Console.WriteLine(Math.Pow(2, 8)); // Math.Pow is a static class
+
+        }
+
+            // public class DrinksMachine
+            // {
+            //     public DrinksMachine(string loc, string make, string model) // this is our constructor
+            //     {
+            //         this.Location = loc;
+            //         this.Make = make;
+            //         this.Model = model;
+            //     }
+
+
+            //     // private member variables
+            //     private string _location; 
+            //     private string _make;
+            //     private string _model;
+
+            //     // public properties
+            //     public string Location // encapsulating public properties
+            //     { 
+            //         get { return _location; }
+            //         set { _location = value; }
+            //     }
+
+            //     public string Make 
+            //     { 
+            //         get { return _make; }
+            //         set { _make = value; }
+            //     }
+            //     public string Model 
+            //     { 
+            //         get { return _model; }
+            //         set { _model = value; }
+            //     }
+        // }
+
+
+
+    }
+}
